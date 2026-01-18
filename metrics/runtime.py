87b827eb3034a631badf3492fa0fd04335e28827
Astrosynx <berni233202@gmail.com>
2026-01-18 16:05:08 +0700

    Update runtime.py

--- metrics/runtime.py
+++ metrics/runtime.py
@@ -1,18 +1,24 @@
 import psutil
+from metrics.grpc_client import StoryGRPCClient
 
 class RuntimeMetrics:
     def __init__(self):
         self.start_peers = self.peers()
+        self.grpc = StoryGRPCClient()
 
     def peers(self):
         return len(psutil.net_connections())
 
     def finish(self, duration):
         end_peers = self.peers()
+        grpc_metrics = self.grpc.collect()
+
         data = {
             "duration_sec": round(duration, 2),
             "peers_before": self.start_peers,
             "peers_after": end_peers,
+            "grpc": grpc_metrics
         }
+
         print("[METRICS]", data)
         return data
